35c9e9e5f020cec1180ca9db4d5e51c91e6c61ea
KaxisIsKaktus <aamanzhol557@gmail.com>
2025-01-25 21:57:40 +0500

    Shooting updated

--- Assets/Project/Scripts/Alikhan/Shooting2.cs
+++ Assets/Project/Scripts/Alikhan/Shooting2.cs
@@ -14,20 +14,13 @@ public class Shooting2 : MonoBehaviour
     public float bulletSpeed = 30.0f;
     public float fireRate = 0.5f; 
 
-    public int maxAmmo = 10;
-    private int currentAmmo; 
-    public float ammoRestoreRate = 1.0f; 
-
-    private bool isShooting = false; 
-    private bool isRestoringAmmo = false; 
+    private bool isShooting = false;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         Cursor.visible = false;
-
-        currentAmmo = maxAmmo; 
     }
 
     // Update is called once per frame
@@ -41,7 +34,7 @@ public class Shooting2 : MonoBehaviour
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         gameObject.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
-        if (Input.GetMouseButton(0) && currentAmmo > 0 && !isShooting)
+        if (Input.GetMouseButton(0) && !isShooting)
         {
             isShooting = true;
             StartCoroutine(ShootContinuously());
@@ -51,16 +44,11 @@ public class Shooting2 : MonoBehaviour
         {
             isShooting = false;
         }
-
-        if (!isShooting && !isRestoringAmmo && currentAmmo < maxAmmo)
-        {
-            StartCoroutine(RestoreAmmo());
-        }
     }
 
     IEnumerator ShootContinuously()
     {
-        while (isShooting && currentAmmo > 0)
+        while (isShooting)
         {
             mousePos = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
             Vector3 difference = mousePos - gameObject.transform.position;
@@ -72,25 +60,8 @@ public class Shooting2 : MonoBehaviour
 
             fireBullet(direction, rotationZ);
 
-            currentAmmo--;
-
             yield return new WaitForSeconds(fireRate);
         }
-
-        isShooting = false;
-    }
-
-    IEnumerator RestoreAmmo()
-    {
-        isRestoringAmmo = true;
-
-        while (!isShooting && currentAmmo < maxAmmo)
-        {
-            yield return new WaitForSeconds(ammoRestoreRate);
-            currentAmmo++; 
-        }
-
-        isRestoringAmmo = false;
     }
 
     void fireBullet(Vector2 direction, float rotationZ)
